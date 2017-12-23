49410170b308ff5d16477689121311f5b543fac5
Nicolas Mine <nicolas.mine11@gmail.com>
2017-12-23 02:57:48 +0100

    add repository to store mood and comment a notification is display if a mood is present in storagePref for the mood of the day

--- src/main/java/be/nmine/moodtracker/service/AutoSaveMoodReceiver.java
+++ src/main/java/be/nmine/moodtracker/service/AutoSaveMoodReceiver.java
@@ -1,14 +1,14 @@
 package be.nmine.moodtracker.service;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 
-import be.nmine.moodtracker.model.Moods;
+import be.nmine.moodtracker.R;
 import be.nmine.moodtracker.model.enumModel.Mood;
-import be.nmine.moodtracker.util.Constants;
+import be.nmine.moodtracker.repository.Repository;
 
 /**
  * Created by n1mbus on 14-12-17.
@@ -16,12 +16,43 @@ import be.nmine.moodtracker.util.Constants;
 
 public class AutoSaveMoodReceiver extends BroadcastReceiver {
 
-    private SharedPreferences mPrefreences;
+    private Repository mRepository;
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        mPrefreences = PreferenceManager.getDefaultSharedPreferences(context);
-        Moods moods = Moods.fromJson(mPrefreences.getString(Constants.MOOD_OF_THE_DAY, ""));
-        moods.moodOfDay(Mood.HAPPY);
+        mRepository = (Repository) context.getApplicationContext();
+        String dailyMood = mRepository.getDailyMoodTemp();
+        String dailyComment = mRepository.getDailyCommentTemp();
+        if(dailyMood != null && !dailyMood.isEmpty()) {
+            saveMood(Mood.valueOf(dailyMood));
+            sendNotification(context);
+        }
+        if(dailyComment != null && !dailyComment.isEmpty()) {
+            saveComment(dailyComment);
+        }
+
+    }
+
+    private void saveMood(Mood dailyMood) {
+        mRepository.saveMood(dailyMood,1);
+        mRepository.removeMoodOfTheDayTemp();
+    }
+
+    private void saveComment(String dailyComment) {
+        mRepository.saveComment(dailyComment,1);
+        mRepository.removeCommentdOfTheDayTemp();
+    }
+
+    private void sendNotification(Context context){
+        String NOTIFICATION_ID = "channel_id_01";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Save Done !")
+                .setContentText("Your daily mood has been saved");
+
+
+        NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        nManager.notify(1, builder.build());
     }
 }
